11245ec3ba42168bb5ec20f6312e34ffb0bd0579
bobby1232 <i.roshupkin@rambler.ru>
2026-08-01 15:51:39 +0300

    Assemble Telegram welcome banner asset

---
+++ lib/assets/welcome-banner.ts
@@ -0,0 +1,7 @@
+import part0 from "./welcome-banner-part-0";
+import part1 from "./welcome-banner-part-1";
+import part2 from "./welcome-banner-part-2";
+import part3 from "./welcome-banner-part-3";
+
+export const welcomeBannerDataUrl =
+  `data:image/jpeg;base64,${part0}${part1}${part2}${part3}`;
